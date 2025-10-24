7074a99c804f0a14d4e37a6867bbaa28a343bf08
Slava Haikin <vyachowski@gmail.com>
2025-10-25 02:39:45 +0300

    feat: add first test case

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  expect(Question.new(body: '123')).to_not be_valid
+  it 'should create a new question' do
+    expect(Question.new(title: 'Title', body: 'body')).to be_valid
+  end
+
+  it 'should not create new question' do
+    expect(Question.new(body: '123')).to_not be_valid
+    expect(Question.new(title: '123')).to_not be_valid
+  end
 end
