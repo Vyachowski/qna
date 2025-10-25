5c1dcc8cd1fc162dcd7be891a39c402ddbf0f1f3
Slava Haikin <vyachowski@gmail.com>
2025-10-25 14:45:49 +0300

    feat: add extended tests for answer and question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 describe Question, type: :model do
-  subject { described_class.new(title: 'Title', body: 'Body') }
-
+  it { should have_many(:answers).dependent(:destroy) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
 end
